f28a9db1f18d4606c7702157a1395757a4bcf560
ridwanaam46 <hansamu46@gmail.com>
2018-02-25 15:36:36 +0700

    first commit

---
+++ app/src/androidTest/java/com/example/asusx453sa/muhammadridwan_1202154212_modul3/RecyclerAdapterTest.java
@@ -0,0 +1,21 @@
+package com.example.asusx453sa.muhammadridwan_1202154212_modul3;
+
+import org.junit.Test;
+
+/**
+ * Created by ASUS X453SA on 2/23/2018.
+ */
+
+public class RecyclerAdapterTest {
+    @Test
+    public void onCreateViewHolder() throws Exception {
+    }
+
+    @Test
+    public void onBindViewHolder() throws Exception {
+    }
+
+    @Test
+    public void getItemCount() throws Exception {
+    }
+}
